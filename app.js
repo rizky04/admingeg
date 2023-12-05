ad56b8865304194255a7db96e954ab00740c2429
rizky04 <carlivanhowten@gmail.com>
2023-12-05 17:02:34 +0700

    upload image

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/uploads')));
 app.use('/adminlte', express.static(path.join(__dirname,'/node_modules/admin-lte/')));
 
 app.use('/', dashboardRouter);
